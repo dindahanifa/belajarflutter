1c75d7fec156e7e287f70ed4f460b5797b8b32dd
dindahanifa <dinda.h33@gmail.com>
2025-05-20 10:42:43 +0700

    tugas_satu_flutter

---
+++ lib/percobaanflutter.dart
@@ -0,0 +1,85 @@
+// import 'package:flutter/material.dart';
+
+// class MeetSatu extends StatelessWidget {
+//   const MeetSatu({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: const Color.fromARGB(255, 24, 220, 255),
+//         title: Text("Flutter 1"),
+//         centerTitle: true,
+//         actions: [
+//           IconButton(
+//             icon: Icon(Icons.search, color: Colors.black12,),
+//             onPressed: () {
+              
+//             },
+//           )
+//         ],
+//       ),
+//       drawer: Drawer(),
+//       backgroundColor: const Color.fromARGB(255, 194, 248, 255),
+//       floatingActionButton: FloatingActionButton(
+//         child: Icon(Icons.message_outlined),
+//         onPressed:(){}),
+//       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.start,
+//         crossAxisAlignment: CrossAxisAlignment.center,
+//         children: [
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceAround,
+//             children: [
+//               Text("Belajar Flutter", style: TextStyle(
+//                 fontSize: 25,
+//                 fontWeight: FontWeight.bold
+//               ),),
+//             ],
+//           ),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceAround,
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               Text("19 Mei 2025", style: TextStyle(
+//                 fontSize: 23,
+//                 fontWeight: FontWeight.normal
+//               ),),
+//             ],
+//           ),
+//           Text("Kelas Mobile Programing", style: TextStyle(
+//             fontSize: 22,
+//             fontWeight: FontWeight.w200,
+//             fontStyle: FontStyle.italic
+//           ),),
+//           Row(
+//             mainAxisAlignment: MainAxisAlignment.center
+//             ,
+//             children: [
+//               Row(
+//                 children: [
+//                   Text("Loop Dart", style: TextStyle(
+//                     fontSize: 24,
+//                     fontWeight: FontWeight.w300,
+//                     color: Colors.pink
+//                   ),),
+//                 ],
+//               ),
+//             ],
+//           ),
+//           Text("Flutter ke 1"),
+//           Row(
+//             children: [
+//               Text("Data 1"),
+//               Text("Data 2"),
+//               Text("Data 3"),
+//               Text("Data 4"),
+//             ],
+//           )
+//           ],
+//       ),
+//     );
+//   }
+// }
